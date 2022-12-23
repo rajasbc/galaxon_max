87ff5fda93f636e1724283947e7744708ebe7bd2
unknown <hakkim@nigsoft.com>
2022-12-23 19:16:53 +0530

    sale to branch page

--- ajaxCalls/add_purchase_order.php
+++ ajaxCalls/add_purchase_order.php
@@ -1,9 +1,10 @@
 <?php
 include '../tables/config.php';
 
-// print_r($_POST);die();
+// error_reporting(E_ALL);
  $obj = new PurchaseOrder();
  $result =  $obj->add_purchase();
+ 
  echo json_encode($result);
 
 
